83289b09ac414e91801016daa27301e789fc319c
philoteer <cdac1234@gmail.com>
2020-05-11 14:26:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,40 @@
-[[ -d livedl ]] || [[ -f livedl ]] && echo "请使用`sudo rm -rf livedl`指令删除livedl文件或文件夹后重试" && exit 1 #git clone需要空文件夹
+[[ -d livedl ]] || [[ -f livedl ]] && echo "Please use`sudo rm -rf livedl`command to delete the livedl file or folder and try again" && exit 1 #git clone Empty folder required
 
-sudo apt update #更新库
-sudo apt -y install curl #安装curl
-sudo apt -y install ffmpeg #安装ffmpeg
+sudo apt update #Update library
+sudo apt -y install curl #Install curl
+sudo apt -y install ffmpeg #Install ffmpeg
 
-#安装python3相关下载工具
-sudo apt -y install python3 ; sudo apt -y install python3-pip ; sudo apt -y install python3-setuptools #安装python3
-pip3 install streamlink ; pip3 install youtube-dl ; pip3 install you-get #安装基于python3的下载工具
-echo 'export PATH=$PATH:/usr/local/bin'>>~/.bashrc #修改默认环境变量，如不希望可以注释掉
+#Install python3 related download tools
+sudo apt -y install python3 ; sudo apt -y install python3-pip ; sudo apt -y install python3-setuptools #Install python3
+pip3 install streamlink ; pip3 install youtube-dl ; pip3 install you-get #Install download tool based on python3
+echo 'export PATH=$PATH:/usr/local/bin'>>~/.bashrc #Modify the default environment variables, you can comment out if you do n’t want to
 export PATH=$PATH:/usr/local/bin
 
-#安装go相关下载工具
-wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz #覆盖安装go环境，如不希望可以注释掉
+#Install go related download tools
+wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz #Override the installation of the go environment, you can comment out if you do n’t want to
 sudo tar -C /usr/local -xzf go1.12.7.linux-amd64.tar.gz ; rm go1.12.7.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin'>>~/.bashrc #修改默认环境变量，如不希望可以注释掉
+echo 'export PATH=$PATH:/usr/local/go/bin'>>~/.bashrc #Modify the default environment variables, you can comment out if you do n’t want to
 export PATH=$PATH:/usr/local/go/bin
 sudo apt -y install git ; sudo apt -y install build-essential
-echo "此处可能需要较长时间，请耐心等待"
-go get github.com/gorilla/websocket ; go get golang.org/x/crypto/sha3 ; go get github.com/mattn/go-sqlite3 ; go get github.com/gin-gonic/gin #安装必要的go库
-git clone https://github.com/himananiito/livedl.git ; cd livedl ; go build src/livedl.go ; rm -r `ls | grep -v "^livedl$"` ; cd .. #编译安装livedl
+echo "It may take a long time here, please be patient"
+go get github.com/gorilla/websocket ; go get golang.org/x/crypto/sha3 ; go get github.com/mattn/go-sqlite3 ; go get github.com/gin-gonic/gin #Install the necessary go library
+git clone https://github.com/himananiito/livedl.git ; cd livedl ; go build src/livedl.go ; rm -r `ls | grep -v "^livedl$"` ; cd .. #Compile and install livedl
 
-#下载文件并赋予权限
+#Download files and grant permissions
 mkdir record
 wget -O "record/record.sh" "https://raw.githubusercontent.com/lovezzzxxx/liverecord/master/record.sh" ; chmod +x record/record.sh
 wget -O "record/record_twitcast.py" "https://raw.githubusercontent.com/lovezzzxxx/liverecord/master/record_twitcast.py" ; chmod +x "record/record_twitcast.py"
 
-#配置自动上传
-curl https://rclone.org/install.sh | bash #配置rclone自动上传
-sudo wget https://raw.githubusercontent.com/MoeClub/OneList/master/OneDriveUploader/amd64/linux/OneDriveUploader -P /usr/local/bin/ #配置onedrive自动上传
+#Configure automatic upload
+curl https://rclone.org/install.sh | bash #Configure rclone to automatically upload
+sudo wget https://raw.githubusercontent.com/MoeClub/OneList/master/OneDriveUploader/amd64/linux/OneDriveUploader -P /usr/local/bin/ #Configure onedrive automatic upload
 sudo chmod +x /usr/local/bin/OneDriveUploader
-go get github.com/iikira/BaiduPCS-Go #配置百度云自动上传
-echo 'export PATH=$PATH:'`echo ~`'/go/bin'>>~/.bashrc #修改默认环境变量，如不希望可以注释掉
+go get github.com/iikira/BaiduPCS-Go #Configure Baidu cloud automatic upload
+echo 'export PATH=$PATH:'`echo ~`'/go/bin'>>~/.bashrc #Modify the default environment variables, you can comment out if you do n’t want to
 source ~/.bashrc
 
-#提示登陆
-echo '请手动运行`source ~/.bashrc`或者重新链接ssh更新环境变量使下列命令生效'
-echo '使用`rclone config`登陆rclone'
-echo '使用`OneDriveUploader -cn -a "打开https://github.com/MoeClub/OneList/tree/master/OneDriveUploader中的相应网页并登录后浏览器地址栏返回的url"`登陆rclone'
-echo '使用`BaiduPCS-Go login -bduss="百度网盘网页cookie中bduss项的值"`登陆BaiduPCS-Go，'
+#Prompt to log in
+echo 'Please manually run `source ~ / .bashrc` or relink ssh to update the environment variables to make the following commands take effect'
+echo 'Log in to rclone using `rclone config`'
+echo 'Use `OneDriveUploader -cn -a" to open the corresponding web page in https://github.com/MoeClub/OneList/tree/master/OneDriveUploader and log in to the URL returned by the browser address bar after logging in "to log in to rclone'
+echo 'Log in to BaiduPCS-Go using `BaiduPCS-Go login -bduss =" value of the bduss item in the Baidu network disk web cookie "'
